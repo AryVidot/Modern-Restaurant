9c06385d5ec15797396ba1c72faeb107f4c84df7
Imac 4 <imac4@imacs-imac.home>
2023-03-01 21:14:19 +0000

    Added Navbar elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { GiHUmburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+import images from "../../constants/images";
+
 import "./Navbar.css";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app_navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#aMenubout">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
