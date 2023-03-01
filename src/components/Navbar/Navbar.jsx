59348fa904566bb5336a7def995a81e5070ec1b4
Imac 4 <imac4@imacs-imac.home>
2023-03-01 22:25:37 +0000

    Fixed typo on menu IDs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                 <a href="#home">Home</a>
               </li>
               <li className="p__opensans">
-                <a href="#aMenubout">About</a>
+                <a href="#about">About</a>
               </li>
               <li className="p__opensans">
                 <a href="#menu">Menu</a>
