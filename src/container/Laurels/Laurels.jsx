c2b713452e42ffce814fdae1bd2ac49273583a73
Imac 4 <imac4@imacs-imac.home>
2023-03-13 14:32:42 +0000

    Added Intro, gallery and find us section and style

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -17,7 +17,14 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
 );
 
 const Laurels = () => (
-  <div className="app__bg app__wrapper section__padding" id="awards">
+  <div
+    className="app__bg app__wrapper section__padding app__laurels_awards-badge"
+    id="awards"
+  >
+    <div className="app__laurels_awards-badge_logo">
+      <img src={images.logo} alt="logo" />
+    </div>
+
     <div className="app__wrapper_info">
       <SubHeading title="Awards & recognition" />
       <h1 className="headtext__cormorant">Our Laurels</h1>
